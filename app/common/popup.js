50445e7cab78855e358961d3856e082f6a28ac0a
QingYi-Studio <146829069+QingYi-Studio@users.noreply.github.com>
2024-02-13 14:25:59 +0800

    fix popup (#6)
    * fix popup
    
    ---------
    
    Co-authored-by: Grey Wind <98391767+Grey-Wind@users.noreply.github.com>

--- app/common/popup.js
+++ app/common/popup.js
@@ -1,2 +1,9 @@
 const bs = chrome || browser;
 bs.tabs.create({ url: bs.runtime.getURL('./index.html') }, (tab) => console.log(tab));
+
+window.open("./index.html")
+
+function fLoad(){
+    window.open("./index.html")
+    console.log("fLoad")
+}
